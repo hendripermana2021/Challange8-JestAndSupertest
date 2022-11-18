396f8e47ba107e02f7415310c95c4c485a8d83a6
HendriPermana <hendripermana60@gmail.com>
2022-11-18 18:23:25 +0700

    Update Config Database

--- config/database.js
+++ config/database.js
@@ -1,29 +1,25 @@
+const Sequelize = require('sequelize')
 require("dotenv").config();
 const path = require("path");
 
 const DB_TEST_FILE_PATH = path.join(__dirname, "../db/challange8.sqlite");
 const {
-    DB_USEROL = "postgres",
-    DB_PASSWORDOL = "1",
-    DB_NAMEOL = "bcr_development",
-    DB_HOSTOL = "127.0.0.1",
-    DB_PORTOL = "5432",
     DB_USER,
     DB_PASSWORD,
     DB_NAME,
     DB_HOST,
     DB_PORT,
-    DB_URL,
+    DB_URI
 
   } = process.env;
 
 module.exports = {
     development: {
-        username: DB_USEROL,
-        password: DB_PASSWORDOL,
-        database: DB_NAMEOL,
-        host: DB_HOSTOL,
-        port: DB_PORTOL,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        port: DB_PORT,
         dialect: "postgres",
         ssl: true,
     },
@@ -33,19 +29,6 @@ module.exports = {
         dialect: "sqlite",
     },
     production: {
-        username: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_NAME,
-        host: DB_HOST,
-        port: DB_PORT,
-        url: DB_URL,
-        dialect: "postgres",
-        ssl: true,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
+        url : DB_URI
     },
 };
